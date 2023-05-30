0db9ceace714e8a886c90c9841d16e2e2385e5f0
Merchi-o <martinaorellana90@gmail.com>
2023-05-29 23:28:59 -0300

    feat: implementing header links component

---
+++ app/components/header.tsx
@@ -0,0 +1,27 @@
+import * as NavigationMenu from "@radix-ui/react-navigation-menu";
+
+export const Header = () => {
+  return (
+    <NavigationMenu.Root className="NavigationMenuRoot">
+      <NavigationMenu.List className="NavigationMenuList">
+        <NavigationMenu.Item>
+          <NavigationMenu.Link className="NavigationMenuLink" href="">
+            Menu
+          </NavigationMenu.Link>
+        </NavigationMenu.Item>
+
+        <NavigationMenu.Item>
+          <NavigationMenu.Link className="NavigationMenuLink" href="">
+            Menu
+          </NavigationMenu.Link>
+        </NavigationMenu.Item>
+
+        <NavigationMenu.Item>
+          <NavigationMenu.Link className="NavigationMenuLink" href="">
+            Explorar
+          </NavigationMenu.Link>
+        </NavigationMenu.Item>
+      </NavigationMenu.List>
+    </NavigationMenu.Root>
+  );
+};
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,21 +1,26 @@
 import * as Switch from "@radix-ui/react-switch";
 import type { V2_MetaFunction } from "@remix-run/node";
 import { Link } from "@remix-run/react";
+import { Header } from "../components/header";
 
 export const meta: V2_MetaFunction = () => {
   return [{ title: "New Remix App" }];
 };
 
-
 export default function Index() {
   return (
     <main className="h-screen w-screen items-center justify-center flex">
-      <Link to='join' className='hover:bg-gray-100 py-1 px-2 transition-all rounded-md'>Join</Link>
+      <Link
+        to="join"
+        className="hover:bg-gray-100 py-1 px-2 transition-all rounded-md"
+      >
+        Join
+      </Link>
       <Switch.Root className="SwitchRoot" id="airplane-mode">
         <Switch.Thumb className="SwitchThumb" />
       </Switch.Root>
       <button className="btn btn-primary">Primary button</button>
-    
+      <Header></Header>
     </main>
   );
 }
